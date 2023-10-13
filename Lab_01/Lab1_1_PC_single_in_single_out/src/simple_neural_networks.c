2fdf667283877f851cc0784f92bf08b41393a53d
yange <posterscofield@gmail.com>
2023-10-13 09:10:33 +0000

    finished Lab1

--- Lab_01/Lab1_1_PC_single_in_single_out/src/simple_neural_networks.c
+++ Lab_01/Lab1_1_PC_single_in_single_out/src/simple_neural_networks.c
@@ -2,5 +2,5 @@
 
 int32_t single_in_single_out_nn(int32_t input, int32_t weight) {
    	// TODO: Return the result of multiplication of input and its weight.
-	return 0;
+	return input * weight;
 }
--- Lab_01/Lab1_2_PC_multiple_in_single_out/src/simple_neural_networks.c
+++ Lab_01/Lab1_2_PC_multiple_in_single_out/src/simple_neural_networks.c
@@ -2,13 +2,17 @@
 
 double single_in_single_out_nn(double  input, double weight) {
 	// TODO: Return the result of multiplication of input and its weight.
-   	return 0;
+   	return input * weight;
 }
 
 
 double weighted_sum(double * input, double * weight, uint32_t INPUT_LEN){
 	double output = 0;
 	// TODO: Use for loop to multiply all inputs with their weights
+	for(int i =0; i< INPUT_LEN;i++){
+		output += input[i]*weight[i];
+	}
+	
  return output;
 }
 
@@ -16,5 +20,7 @@ double weighted_sum(double * input, double * weight, uint32_t INPUT_LEN){
 double multiple_inputs_single_output_nn(double * input, double *weight, uint32_t INPUT_LEN){
 	double predicted_value = 0;
 	// TODO: Use weighted_sum function to calculate the output
+	
+	predicted_value =  weighted_sum(input, weight,INPUT_LEN);
 	return predicted_value;
 }
